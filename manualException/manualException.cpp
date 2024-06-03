16dc39b936a67244f60272f1eb9332b07ec260a4
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-06-03 10:36:23 +0700

    try menampilkan kalimat

---
+++ manualException/manualException.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
+
+
+int main()
+{
+    try {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+
+    }
+   
+}
+
